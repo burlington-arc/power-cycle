331dc0ef6ccbbee80e5d30e79931e14b29fdb80d
Greg Trasuk <trasukg@trasuk.com>
2016-07-13 09:18:23 -0400

    Added code to daemonize before initiating power cycle.
    This feature is required so as to avoid a failure condition where the
    item is shut off, and then we lose communications, stopping the program.
    That sequence would leave the item shut off.  If the item was the router, we'd
    be unable to communicate further to turn it back on.

--- powerpi/__init__.py
+++ powerpi/__init__.py
@@ -1,4 +1,35 @@
 
+import RPi.GPIO as GPIO
+import time
+import sys
+import os
+from daemon import daemon
+
+pins= { 'repeater': 18, 'router': 17 }
+
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    requestedPin=sys.argv[1];
+    if not requestedPin in pins:
+        print("The following powered items are supported")
+        for pin in pins:
+            print("  " + pin)
+        sys.exit(os.EX_USAGE)
+    if len(sys.argv)==3:
+        duration=int(sys.argv[2])
+    else:
+        duration=10
+
+    print("Cycling {0} for {1} seconds.".format(requestedPin, duration))
+    daemon_context=daemon.DaemonContext()
+
+    daemon_context.open()
+
+    pin=pins[requestedPin];
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
